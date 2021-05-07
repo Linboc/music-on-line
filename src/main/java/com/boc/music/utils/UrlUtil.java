c5be18f820231b35ea1685aa2378917ccd977bd5
boc <baichenlin@reabam.com>
2021-05-07 16:06:20 +0800

    跟随酷我网站调整参数

--- src/main/java/com/boc/music/utils/UrlUtil.java
+++ src/main/java/com/boc/music/utils/UrlUtil.java
@@ -8,6 +8,7 @@ import org.springframework.web.client.RestTemplate;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.UUID;
 
 /**
  * url工具类
@@ -32,20 +33,21 @@ public class UrlUtil {
     }
 
     public static String getKuwoPage(String key, String pn, String rn) {
-        String url = String.format("http://kuwo.cn/api/www/search/searchMusicBykeyWord?key=%s&pn=%s&rn=%s", key, pn, rn);
+        String reqId = UUID.randomUUID().toString();
+        String url = String.format("http://kuwo.cn/api/www/search/searchMusicBykeyWord?key=%s&pn=%s&rn=%s&reqId=%s", key, pn, rn, reqId);
         HttpHeaders headers = new HttpHeaders();
         headers.add("Referer", "kuwo.cn");
         headers.add("Cookie", "kw_token=a");
         headers.add("csrf", "a");
         HttpEntity<String> requestEntity = new HttpEntity((Object)null, headers);
-        return (String)CLIENT.exchange(url, HttpMethod.GET, requestEntity, String.class, new Object[0]).getBody();
+        return CLIENT.exchange(url, HttpMethod.GET, requestEntity, String.class, new Object[0]).getBody();
     }
 
     public static String getAcFunM3u8Url(String url) {
         HttpHeaders headers = new HttpHeaders();
         headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36");
         HttpEntity<String> requestEntity = new HttpEntity((Object)null, headers);
-        return (String)CLIENT.exchange(url, HttpMethod.GET, requestEntity, String.class, new Object[0]).getBody();
+        return CLIENT.exchange(url, HttpMethod.GET, requestEntity, String.class, new Object[0]).getBody();
     }
 
 }
